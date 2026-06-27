02a8ac8d196c05d4add57c1cd701c27813b8a2de
lianamahesra-creator <lianamahesra@gmail.com>
2026-06-28 06:49:10 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
+set -e
 
 export DISPLAY=:1
 
+mkdir -p ~/.vnc
+
+cat > ~/.vnc/xstartup <<'EOF'
+#!/bin/sh
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+exec startxfce4
+EOF
+
+chmod +x ~/.vnc/xstartup
+
+echo password | vncpasswd -f > ~/.vnc/passwd
+chmod 600 ~/.vnc/passwd
+
 Xvfb :1 -screen 0 1920x1080x24 &
 
-vncserver :1 -geometry 1920x1080 -depth 24
+vncserver :1 \
+    -geometry 1920x1080 \
+    -depth 24 \
+    -localhost no
 
-websockify --web=/usr/share/novnc 6080 localhost:5901 &
+websockify \
+    --web=/usr/share/novnc \
+    6080 \
+    localhost:5901
 
-startxfce4 &
+wait
